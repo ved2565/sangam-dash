8101e41f4ef7e02fea87b91f37f27e225ed3fdb7
Lazycoder26 <Rudra26_2004@outlook.com>
2023-10-27 15:17:22 +0530

    added lenis js and some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Lenis from '@studio-freight/lenis'
 
-function App() {
-  return (
-    <div>App</div>
-  )
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
 }
 
-export default App
+requestAnimationFrame(raf)
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="section1 min-h-screen bg-slate-300">Section 1</div>
+            <div className="section2 min-h-screen bg-slate-600">Section 2</div>
+            <div className="section3 min-h-screen bg-slate-900">Section 3</div>
+        </div>
+    );
+};
+
+export default App;
