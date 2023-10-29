07a3b1d91ae26fea83eab0bbb76b727b17047b55
Jems Patel <jemspatel2004@gmail.com>
2023-10-29 15:12:46 +0530

    jems

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,9 @@ const About = () => {
   return (
     <div>
       <p className='text-4xl'>About</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet dolores delectus sit illum odio? Et, tempora animi in, earum iusto magni sed iure quia molestias rem corporis commodi perferendis blanditiis?
+      </p>
       <Link to="/">Home</Link>
     </div>
   )
