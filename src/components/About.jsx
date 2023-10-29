6c84b4134cb0274bcf56bec1708c8eba1d29337f
Lazycoder26 <Rudra26_2004@outlook.com>
2023-10-29 14:39:26 +0530

    a minor change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <p className='text-4xl'>About</p>
       <Link to="/">Home</Link>
     </div>
   )
