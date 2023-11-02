9fe5c556a352968463e2c6d8bba6086bbf956d51
Lazycoder26 <Rudra26_2004@outlook.com>
2023-11-02 22:05:18 +0530

    Update API endpoints for login and registration

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -10,7 +10,7 @@ const LoginPage = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.post("http://localhost:5000/login", {
+      const response = await axios.post("http://localhost:6969/login", {
         username: email, // Assuming your server expects 'username' instead of 'email'
         password,
       });
