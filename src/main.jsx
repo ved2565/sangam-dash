a42f8ccebbee1b472b8d2ef2fde7f1e192c2ee03
Vedant Pandey <vedantpandey434@gmail.com>
2023-11-04 15:32:36 +0530

    scheme page added

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ import RegisterPage from "./components/RegisterPage.jsx";
 import UploadCSV from "./components/UploadCSV.jsx";
 import UserProfile from "./components/UserProfile.jsx";
 import Demography from "./components/Demography.jsx";
+import AddSchemes from "./components/AddSchmes.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -28,6 +29,7 @@ const router = createBrowserRouter(
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/profile" element={<UserProfile />} />
+      <Route path="/addScheme" element={<AddSchemes />} />
     </Route>
   )
 );
